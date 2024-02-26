c3f991ac1af4d4035742468b88ab1e8fbbdc6dac
Cornelius Schumacher <schumacher@kde.org>
2024-02-26 23:39:30 +0100

    Import historic code

---
+++ gnuclad/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "gnuclad"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "donjan at dyx dot ch"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gnuclad"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gnuclad HEAD"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gnuclad"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "HEAD"
+
+/* Version number of package */
+#define VERSION "HEAD"
