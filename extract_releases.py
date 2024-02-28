f645b0ae9fefa24eb4ff732d5824990373bd73ab
Cornelius Schumacher <cornelius.schumacher@deutschebahn.com>
2024-02-28 21:28:11 +0100

    Identify most releases

--- extract_releases.py
+++ extract_releases.py
@@ -2,12 +2,69 @@
 
 from pathlib import Path
 from bs4 import BeautifulSoup
+from datetime import datetime
+import re
 
 page = 1
 
+def identify_release(title):
+    pre_matches = ["Release Candidate", "Beta", "beta", "Alpha", " RC"]
+    if any(x in title for x in pre_matches):
+        return "pre", None
+
+    norelease_matches = ["conf.kde.in", "Summer of Code", "Eco-Certified", "PinePhone", "Purism", "Forum", "Wikimedia", "aKademy", "ZD Innovation", "Delix", "SuSE", "Support Of KDE", "CeBIT", "SourceForge", "LinuxWorld", "GNOME", "Stallman", "Linux Congress", "KDE Project Announced", "FSFE", "KDE 2 Launch Pad", "Corel"]
+    if any(x in title for x in norelease_matches):
+        return "norelease", None
+
+    m = re.search("KDE Gear\D+([\d\.]+)", title)
+    if m:
+        return "gear", m.group(1)
+
+    m = re.search("Frameworks (.*)", title)
+    if m:
+        return "frameworks", m.group(1)
+
+    m = re.search("Plasma (.*)", title)
+    if m:
+        return "plasma", m.group(1)
+
+    m = re.search("([\d\.]+) Releases", title)
+    if m:
+        return "releases", m.group(1)
+
+    m = re.search("KDE's (.*) Apps Update", title)
+    if m:
+        return "apps-update", m.group(1)
+
+    m = re.search("Applications ([\d\.]+)", title)
+    if m:
+        return "applications", m.group(1)
+
+    m = re.search("Kirigami (.*)", title)
+    if m:
+        return "kirigami", m.group(1)
+
+    m = re.search("Applications and Platform ([\d\.]+)", title)
+    if m:
+        return "applications-platform", m.group(1)
+
+    m = re.search("Software Compilation (.*)", title)
+    if m:
+        return "compilation", m.group(1)
+
+    m = re.search("KDE (.*) Release", title)
+    if m:
+        return "kde", m.group(1)
+
+    m = re.search("KOffice (.*) Release", title)
+    if m:
+        return "koffice", m.group(1)
+
+    return None, None
+
 csv_path = Path("all-kde-releases.csv")
 with csv_path.open("w") as csv:
-    csv.write("Day,Date,Title,Link\n")
+    csv.write("Date,Title,Link\n")
     for page in range(1,76):
         path = Path("announcements") / f"page-{page:02d}.html"
 
@@ -20,12 +77,15 @@ with csv_path.open("w") as csv:
             for release in main.findAll("div", recursive=False):
                 date = None
                 title = None
+                stream = None
                 link = None
                 for element in release.findChildren():
                     if element["class"][0] == "text-lighter":
-                        date = element.text
+                        date_str = element.text.split(",")[1]
+                        date = datetime.strptime(date_str, " %d %B %Y").date()
                     if element.name == "h3":
-                        title = element.text
+                        title = element.text.replace("\n"," ")
+                        stream, version = identify_release(title)
                     if element["class"][0] == "post-entry":
                         link = element.find("a")["href"]
-                csv.write(f'{date},"{title}",{link}\n')
+                csv.write(f'{stream},{version},{date},"{title}",{link}\n')
